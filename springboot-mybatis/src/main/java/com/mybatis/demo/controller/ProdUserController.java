78db3e7c792e1fac67ba32c31705657f5b6d02a8
longxinwu <1043547002@qq.com>
2021-06-14 23:23:41 +0800

    1 添加日志打印相关配置

--- springboot-mybatis/src/main/java/com/mybatis/demo/controller/ProdUserController.java
+++ springboot-mybatis/src/main/java/com/mybatis/demo/controller/ProdUserController.java
@@ -1,6 +1,8 @@
 package com.mybatis.demo.controller;
 
 import com.mybatis.demo.Service.ProdUserService;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/demo")
+@Slf4j
 public class ProdUserController {
 
     @Autowired
     ProdUserService prodUserService;
     @GetMapping("/getUser")
     public String getProdUser(int id){
+        log.info("info级别的日志");
+        log.warn("warn级别的日志");
+        log.error("error级别的日志");
         return prodUserService.getUserInfo(id).toString();
     }
 }
